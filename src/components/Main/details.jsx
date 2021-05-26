19a855b558958442a8aa108dadf0c30ca99bb9ca
Nibir Ray <nibirrayy@gmail.com>
2021-05-27 05:27:59 +0530

    changes in this commit: - added complete password rendering

--- src/components/Main/details.jsx
+++ src/components/Main/details.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+
+import { vaultContext } from './main.jsx'
 
 export default function Details() {
+
+    const { vaultData, selectedPassword, selectedCategory } = useContext(vaultContext)
+
+
     return (
         <section
             className="relative w-4/12 flex-1 bg-gradient-to-r via-white from-gray-100 to-gray-100 bg-opacity-80 shadow-2xl">
@@ -10,25 +16,51 @@ export default function Details() {
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
             </svg>
-            <form className=" mt-14 mb-20">
-                <label className="hover:text-gray-600 ml-24 " for="title"><b>TITLE</b></label>
-                <input className="rounded-lg ml-10 ring-2" type="text" name="title" placeholder="Enter a Title" /><br />
-                <label className="hover:text-gray-600 ml-12 " for="uname"><b>USERNAME</b></label>
-                <input className="rounded-lg ml-10 mt-5 ring-2" type="text" name="uname" placeholder="Enter a username" /><br />
-                <label className="hover:text-gray-600 ml-12 " for="pword"><b>PASSWORD</b></label>
-                <input className="rounded-lg ml-10 mt-5 ring-2" type="password" name="pword" placeholder="Enter password" />
-            </form>
+            {
 
-            <div className="justify-center mb-20">
-                <h1 className="ml-8 mb-5 text-gray-600"><b>CUSTOM TAGS</b></h1>
-                <form>
-                    <label className="hover:text-gray-600 ml-28" for="url"><b>URL</b></label>
-                    <input className="rounded-lg ml-10 mt-5 ring-2" type="text" name="url" placeholder="Enter a URL" /><br />
-                    <label className="hover:text-gray-600 ml-28" for="pin"><b>PIN</b></label>
-                    <input className="rounded-lg ml-10 mt-5 ring-2" type="text" name="pin" placeholder="Enter a PIN" />
+                vaultData.map((category, index) => {
+                    if (index == selectedCategory - 1) {
+                        return (
+                            category.entries.map((password_items) => {
+                                if (selectedPassword == password_items.password_id) {
+                                    return (
+                                        <form className=" mt-14 mb-20">
+                                            <label className="hover:text-gray-600 ml-24 " for="title"><b>TITLE</b></label>
+                                            <input className="rounded-lg ml-10 ring-2" type="text" name="title" placeholder="Enter a Title" value={password_items.title} /><br />
+                                            <label className="hover:text-gray-600 ml-12 " for="uname"><b>USERNAME</b></label>
+                                            <input className="rounded-lg ml-10 mt-5 ring-2" type="text" name="uname" placeholder="Enter a username" value={password_items.username} /><br />
+                                            <label className="hover:text-gray-600 ml-12 " for="pword"><b>PASSWORD</b></label>
+                                            <input className="rounded-lg ml-10 mt-5 ring-2" type="text" name="pword" placeholder="Enter password" value={password_items.password} />
+                                        </form>
+                                    )
+                                }
+                            })
+                        )
+
+                    }
+                })
+            /*
+            
+                <form className=" mt-14 mb-20">
+                    <label className="hover:text-gray-600 ml-24 " for="title"><b>TITLE</b></label>
+                    <input className="rounded-lg ml-10 ring-2" type="text" name="title" placeholder="Enter a Title" /><br />
+                    <label className="hover:text-gray-600 ml-12 " for="uname"><b>USERNAME</b></label>
+                    <input className="rounded-lg ml-10 mt-5 ring-2" type="text" name="uname" placeholder="Enter a username" /><br />
+                    <label className="hover:text-gray-600 ml-12 " for="pword"><b>PASSWORD</b></label>
+                    <input className="rounded-lg ml-10 mt-5 ring-2" type="password" name="pword" placeholder="Enter password" />
                 </form>
+            */}
+            {/*
+                <div className="justify-center mb-20">
+                    <h1 className="ml-8 mb-5 text-gray-600"><b>CUSTOM TAGS</b></h1>
+                    <form>
+                        <label className="hover:text-gray-600 ml-28" for="url"><b>URL</b></label>
+                        <input className="rounded-lg ml-10 mt-5 ring-2" type="text" name="url" placeholder="Enter a URL" /><br />
+                        <label className="hover:text-gray-600 ml-28" for="pin"><b>PIN</b></label>
+                        <input className="rounded-lg ml-10 mt-5 ring-2" type="text" name="pin" placeholder="Enter a PIN" />
+                    </form>
 
-            </div>
+                </div>*/}
             <button id="button" type="button"
                 className="rounded-full pt-1 mt-7 ml-5 h-8 w-40 text-sm text-white flex  align-text-bottom transition-all duration-150 ease-linear rounded-xl shadow outline-none bg-gray-500 hover:bg-gray-600 hover:shadow-lg focus:outline-none justify-center shadow-2xl">
                 <b>+ ADD NEW FIELD</b>
